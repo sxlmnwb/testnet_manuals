61a2e90182ba4b5b858428d0831050f8b35b9216
Sergio | PPNV <ppnv.contact@gmail.com>
2022-11-12 04:35:10 +0000

    update: neutron - quark-1 testnet v0.1.1
    - add new persistent peers from PPNV Service
    - add addrbook.json from PPNV Service
    
    Signed-off-by: sergiomateiko <ppnv.contact@gmail.com>
    Signed-off-by: Salman Wahib <sxlmnwb.dev@gmail.com>

--- neutron/quark-1/quark-1.sh
+++ neutron/quark-1/quark-1.sh
@@ -2,7 +2,7 @@
 # // Copyright (C) 2022 Salman Wahib (sxlmnwb)
 #
 
-echo -e "\033[0;31m"  
+echo -e "\033[0;31m"
 echo "  ██████ ▒██   ██▒ ██▓     ███▄ ▄███▓ ███▄    █  █     █░▓█████▄ ";
 echo "▒██    ▒ ▒▒ █ █ ▒░▓██▒    ▓██▒▀█▀ ██▒ ██ ▀█   █ ▓█░ █ ░█▒██▒ ▄██░";
 echo "░ ▓██▄   ░░  █   ░▒██░    ▓██    ▓██░▓██  ▀█ ██▒▒█░ █ ░█ ▒██░█▀  ";
@@ -57,13 +57,15 @@ neutrond init "$NODENAME" --chain-id quark-1
 
 # Genesis File
 curl -s https://raw.githubusercontent.com/neutron-org/testnets/main/quark/genesis.json > ~/.neutrond/config/genesis.json
+curl -s https://raw.githubusercontent.com/sergiomateiko/addrbooks/main/neutron/addrbook.json > ~/.neutrond/config/addrbook.json
 
 # Genesis sha256
 sha256sum ~/.neutrond/config/genesis.json
+sha256sum ~/.neutrond/config/addrbook.json
 
 #Set Seeds And Peers
 SEEDS="e2c07e8e6e808fb36cca0fc580e31216772841df@seed-1.quark.ntrn.info:26656,c89b8316f006075ad6ae37349220dd56796b92fa@tenderseed.ccvalidators.com:29001"
-PEERS="fcde59cbba742b86de260730d54daa60467c91a5@23.109.158.180:26656,5bdc67a5d5219aeda3c743e04fdcd72dcb150ba3@65.109.31.114:2480,3e9656706c94ae8b11596e53656c80cf092abe5d@65.21.250.197:46656,9cb73281f6774e42176905e548c134fc45bbe579@162.55.134.54:26656,27b07238cf2ea76acabd5d84d396d447d72aa01b@65.109.54.15:51656,f10c2cb08f82225a7ef2367709e8ac427d61d1b5@57.128.144.247:26656,20b4f9207cdc9d0310399f848f057621f7251846@222.106.187.13:40006,5019864f233cee00f3a6974d9ccaac65caa83807@162.19.31.150:55256,2144ce0e9e08b2a30c132fbde52101b753df788d@194.163.168.99:26656,b37326e3acd60d4e0ea2e3223d00633605fb4f79@nebula.p2p.org:26656"
+PEERS="3f4ecffa24ad9cc38b3aa44aee837cbf660640c4@rpc.neutron.ppnv.space:11656"
 sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.neutrond/config/config.toml
 
 #Set Minimum Gas Price
