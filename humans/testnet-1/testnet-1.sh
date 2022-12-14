da272d38e8b15add3ecfda8563d63039fb3862ea
Salman Wahib <sxlmnwb@sxlzptprjkt.xyz>
2022-12-14 11:53:36 +0000

    update: humans/testnet-1 add statesync and
    - fix missing gas
    - update new persistent peers
    - add new RPC from PPNV Service
    
    Signed-off-by: sergiomateiko <ppnv.contact@gmail.com>
    Signed-off-by: Salman Wahib <sxlmnwb@sxlzptprjkt.xyz>

--- humans/testnet-1/testnet-1.sh
+++ humans/testnet-1/testnet-1.sh
@@ -21,7 +21,7 @@ HEART_WALLET=wallet
 HEART=humansd
 HEART_ID=testnet-1
 HEART_FOLDER=.humans
-HEART_VER=v1
+HEART_VER=v1.0.0
 HEART_REPO=https://github.com/humansdotai/humans
 HEART_GENESIS=https://rpc-testnet.humans.zone/genesis
 #HEART_ADDRBOOK=
@@ -88,7 +88,7 @@ $HEART init $HEART_NODENAME --chain-id $HEART_ID
 curl -Ls $HEART_GENESIS | jq -r .result.genesis > $HOME/$HEART_FOLDER/config/genesis.json
 
 # Set peers and seeds
-PEERS="1df6735ac39c8f07ae5db31923a0d38ec6d1372b@45.136.40.6:26656,9726b7ba17ee87006055a9b7a45293bfd7b7f0fc@45.136.40.16:26656,6e84cde074d4af8a9df59d125db3bf8d6722a787@45.136.40.18:26656,eda3e2255f3c88f97673d61d6f37b243de34e9d9@45.136.40.13:26656,4de8c8acccecc8e0bed4a218c2ef235ab68b5cf2@45.136.40.12:26656"
+PEERS="3fc2c2e3a4b11d540c736a4ae4c9c247fb05fbae@168.119.186.161:26656,3d1e89341f64df76599748b634cbabbb8ba3d1b2@65.21.170.3:43656,295be5393e99c60763c85987fa3f8045af20d828@95.214.53.178:36656,69822c67487d4907f162fdd6d42549e1df60c82d@65.21.224.248:26656,c2cf4e1d0da9ac1e8be5d5288d0bf8e8052b2d86@65.109.92.148:60856,fa57a5bd809eb234f0135e2e62039b5ea09d3992@65.108.250.241:36656,5c27e54b2b8a597cbbd1c43905d2c18a67637644@142.132.231.118:36656,c7181941789884d6c468bfca31778b10f83a388e@95.217.12.217:26656,c40acba57194521c2d16d59e9dcb2250bb8f2db2@162.55.245.219:36656,981e9829afd1679cd9fafc90edc4ff918057e6fe@217.13.223.167:60556,5e41a64298ca653af5297833c6a47eb1ad1bf367@154.38.161.212:36656,3f13ad6e8795479b051d147a5049bf4bd0a63817@65.108.142.47:22656,aac683209559ca9ea48de4c47f3806483a5ec13f@185.244.180.97:26656,d55876bc04e363bbe68a7fb344dd65632e310f45@138.201.121.185:26668"
 sed -i.default "s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/;" $HOME/$HEART_FOLDER/config/config.toml
 
 # Set Port
@@ -117,7 +117,7 @@ sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every
 sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/$HEART_FOLDER/config/app.toml
 
 # Set minimum gas price
-sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.01$QCK_DENOM\"/" $HOME/$QCK_FOLDER/config/app.toml
+sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.01$HEART_DENOM\"/" $HOME/$HEART_FOLDER/config/app.toml
 
 # Reset network
 $HEART tendermint unsafe-reset-all --home $HOME/$HEART_FOLDER
