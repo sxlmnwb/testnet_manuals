4e4ff56bd9664fa89acc26404fb101b0181ce494
Polkachu Intern <hello@polkachu.com>
2022-12-28 05:59:31 +0000

    update: humans/testnet-1 regex to snapshots on installer
    - snapshots by polkachu
    
    Signed-off-by: Polkachu Intern <hello@polkachu.com>
    Signed-off-by: Salman Wahib <sxlmnwb@sxlzptprjkt.xyz>

--- humans/testnet-1/testnet-1.sh
+++ humans/testnet-1/testnet-1.sh
@@ -2,7 +2,7 @@
 # // Copyright (C) 2022 Salman Wahib (sxlmnwb)
 #
 
-echo -e "\033[0;31m"  
+echo -e "\033[0;31m"
 echo "  ██████ ▒██   ██▒ ██▓     ███▄ ▄███▓ ███▄    █  █     █░▓█████▄ ";
 echo "▒██    ▒ ▒▒ █ █ ▒░▓██▒    ▓██▒▀█▀ ██▒ ██ ▀█   █ ▓█░ █ ░█▒██▒ ▄██░";
 echo "░ ▓██▄   ░░  █   ░▒██░    ▓██    ▓██░▓██  ▀█ ██▒▒█░ █ ░█ ▒██░█▀  ";
@@ -23,7 +23,7 @@ HEART_ID=testnet-1
 HEART_FOLDER=.humans
 HEART_VER=v1.0.0
 HEART_REPO=https://github.com/humansdotai/humans
-HEART_GENESIS=https://rpc-testnet.humans.zone/genesis
+HEART_GENESIS=https://snapshots.polkachu.com/testnet-genesis/humans/genesis.json
 HEART_ADDRBOOK=https://snapshots.polkachu.com/testnet-addrbook/humans/addrbook.json
 HEART_DENOM=uheart
 HEART_PORT=12
@@ -42,8 +42,8 @@ source $HOME/.bash_profile
 
 # Set Vars
 if [ ! $HEART_NODENAME ]; then
-	read -p "sxlzptprjkt@w00t666w00t:~# [ENTER YOUR NODE] > " HEART_NODENAME
-	echo 'export HEART_NODENAME='$HEART_NODENAME >> $HOME/.bash_profile
+        read -p "sxlzptprjkt@w00t666w00t:~# [ENTER YOUR NODE] > " HEART_NODENAME
+        echo 'export HEART_NODENAME='$HEART_NODENAME >> $HOME/.bash_profile
 fi
 echo ""
 echo -e "YOUR NODE NAME : \e[1m\e[31m$HEART_NODENAME\e[0m"
@@ -85,14 +85,12 @@ $HEART config node tcp://localhost:${HEART_PORT}657
 $HEART init $HEART_NODENAME --chain-id $HEART_ID
 
 # Download genesis and addrbook
-curl -Ls $HEART_GENESIS | jq -r .result.genesis > $HOME/$HEART_FOLDER/config/genesis.json
+wget $HEART_GENESIS -O $HOME/$HEART_FOLDER/config/genesis.json
 wget $HEART_ADDRBOOK -O $HOME/$HEART_FOLDER/config/addrbook.json
 
 # Set peers and seeds
 SEEDS=ade4d8bc8cbe014af6ebdf3cb7b1e9ad36f412c0@testnet-seeds.polkachu.com:18456
-PEERS=b2c4407beadead20cad5f8dd541bc70b289e1819@65.109.90.33:18456
-sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/$HEART_FOLDER/config/config.toml
-sed -i.bak -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/$HEART_FOLDER/config/config.toml
+sed -i -e "s|^seeds *=.*|seeds = \"$SEEDS\"|" $HOME/$HEART_FOLDER/config/config.toml
 
 # Set Port
 sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:${HEART_PORT}658\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:${HEART_PORT}657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:${HEART_PORT}060\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:${HEART_PORT}656\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":${HEART_PORT}660\"%" $HOME/$HEART_FOLDER/config/config.toml
@@ -112,7 +110,7 @@ CONFIG_TOML="$HOME/.humans/config/config.toml"
 # Set Config Pruning
 pruning="custom"
 pruning_keep_recent="100"
-pruning_keep_every="2000"
+pruning_keep_every="0"
 pruning_interval="10"
 sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $HOME/$HEART_FOLDER/config/app.toml
 sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $HOME/$HEART_FOLDER/config/app.toml
@@ -126,27 +124,9 @@ sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.01$HEART_DENOM\"/
 indexer="null" && \
 sed -i -e "s/^indexer *=.*/indexer = \"$indexer\"/" $HOME/$HEART_FOLDER/config/config.toml
 
-# State sync
-cd $HOME
-sed -i -e "s/^snapshot-interval *=.*/snapshot-interval = \"2000\"/" $HEART_FOLDER/config/app.toml
-sed -i -e "s/^snapshot-keep-recent *=.*/snapshot-keep-recent = \"5\"/" $HEART_FOLDER/config/app.toml
-
-cp $HOME/$HEART_FOLDER/data/priv_validator_state.json $HOME/$HEART_FOLDER/priv_validator_state.json.backup
-
-$HEART tendermint unsafe-reset-all --home $HOME/$HEART_FOLDER --keep-addr-book
-
-SNAP_RPC="https://humans-testnet-rpc.polkachu.com:443"
-
-LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height); \
-BLOCK_HEIGHT=$((LATEST_HEIGHT - 2000)); \
-TRUST_HASH=$(curl -s "$SNAP_RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
-
-sed -i.bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
-s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC,$SNAP_RPC\"| ; \
-s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
-s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"|" $HOME/$HEART_FOLDER/config/config.toml
-
-mv $HOME/$HEART_FOLDER/priv_validator_state.json.backup $HOME/$HEART_FOLDER/data/priv_validator_state.json
+# Enable snapshots
+rm -rf $HOME/$HEART_FOLDER/data
+curl -L https://snapshots.polkachu.com/testnet-snapshots/humans/humans_478320.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/$HEART_FOLDER
 
 # Create Service
 sudo tee /etc/systemd/system/$HEART.service > /dev/null <<EOF
